7559681005a1d407fb8b7242ef9908f7bd74ddf0
Kaviya R <rkaviyakaviya2005@gmail.com>
2026-09-20 18:04:59 +0530

    Add files via upload

---
+++ color.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define BLUE_COLOR "\033[34m"
+#define RESET_COLOR "\033[0m"
+#define RED_COLOR "\033[31m"
+#define GREEN_COLOR "\033[32m"
+#define YELLOW_COLOR "\033[33m"
+#define CYAN_COLOR "\033[36m"
+
+#endif
